0fac41e2b4769ac6e91655424fa266d5617cd9fc
IacovosP <IacovosP@users.noreply.github.com>
2020-09-10 00:04:24 +0100

    Added unit tests for album-page

--- indiestuff/src/app/album/album-page.component.ts
+++ indiestuff/src/app/album/album-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ElementRef, Injectable } from "@angular/core";
-import { ArtistMusic } from "@src/app/music-types/artistMusic";
 import httpClient from "../network/HttpClient";
-import { Album, Track } from "../music-types/types";
+import { Album } from "../music-types/types";
 import {ActivatedRoute} from '@angular/router';
+import { promises } from "dns";
 
 @Component({
   selector: "app-album-page",
@@ -15,21 +15,23 @@ export class AlbumPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log("route: " + JSON.stringify(this.route));
     this.route.params.subscribe(param => {
       console.log("params : " + JSON.stringify(param));
       this.loadPage(param.id);
     });
+    console.log("here 2 " + JSON.stringify(this.route));
     const albumId = String(this.route.snapshot.params.id);
     this.loadPage(albumId);
-
   }
 
   loadPage(albumId: string) {
+    console.log("here calling");
     httpClient
       .fetch("album/" + albumId)
       .then((response) => {
         console.log("response for album: " + JSON.stringify(response));
-        response.tracks = response.tracks.map((track) => {
+        const tracks = response.tracks.map((track) => {
           return {
             ...track,
             fileName: track.filename,
@@ -37,7 +39,7 @@ export class AlbumPageComponent implements OnInit {
             durationInSec: track.durationInSeconds,
           };
         });
-        this.album = response;
+        this.album = {...response, tracks};
         this.album.imageUrl =
           "https://indie-image-test.s3.eu-west-2.amazonaws.com/" +
           response.album_image_filename;
