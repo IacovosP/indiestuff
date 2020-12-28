49ded92e7773bea68c9821ff70a2c445bd6de6d0
unknown <pantekhis_iacovos@hotmail.com>
2020-12-28 22:38:48 +0200

    Removed rest from monorepo

--- rest-api-indiestuff/src/routes/home.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import HomePageController from "../controllers/HomePageController";
-
-const router = Router();
-
-router.get("/", HomePageController.getHomePage)
-
-export default router;
--- rest-api-indiestuff/src/routes/search.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import SearchController from "../controllers/SearchController";
-
-const router = Router();
-
-router.post("/", SearchController.find);
-
-export default router;
