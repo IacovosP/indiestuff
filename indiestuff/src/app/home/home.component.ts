10c99be21d02d055f39fb42ed87824f7fe110f45
unknown <pantekhis_iacovos@hotmail.com>
2021-01-02 12:49:20 +0200

    Add track-list header and add home-page image resizing

--- indiestuff/src/app/home/home.component.ts
+++ indiestuff/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import defaultHttpClient from "@src/app/network/DefaultHttpClient";
 import { HomePageAlbumInterface } from "@src/app/music-types/lib";
 import auth from "../auth/Auth";
@@ -11,11 +11,23 @@ import auth from "../auth/Auth";
 export class HomePageComponent implements OnInit {
   albums: HomePageAlbumInterface[];
   topAlbum: HomePageAlbumInterface;
-
+  innerWidthMain: string;
+  innerWidthSide: string;
+  innerWidthSideForTop: string;
   constructor() {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidthMain = window.innerWidth ? (window.innerWidth / 3).toString() + 'px' : '800px';
+    this.innerWidthSide = window.innerWidth ? (window.innerWidth / 6).toString() + 'px' : '398px';
+    this.innerWidthSideForTop = window.innerWidth ? (window.innerWidth / 12).toString() + 'px' : '199px';
+  }
 
   ngOnInit() {
     this.loadHomePage();
+    this.innerWidthMain = window.innerWidth ? (window.innerWidth / 3).toString() + 'px' : '800px';
+    this.innerWidthSide = window.innerWidth ? (window.innerWidth / 6).toString() + 'px' : '398px';
+    this.innerWidthSideForTop = window.innerWidth ? (window.innerWidth / 12).toString() + 'px' : '199px';
   }
 
   loadHomePage() {
